b9f29b183c45212ce98ec8622b6844c0dadf6af4
lucasnahuelacuna <lucas.nahuel.acuna@gmail.com>
2022-05-01 17:29:06 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>Native Modules React Native</Text>
+    </View>
+  )
+}
+
+export default App
